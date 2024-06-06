c9029cf74a69ff00a9ff3d7a6679525a64a0e447
Brendan Roley <ochyusspeed@gmail.com>
2024-06-05 20:46:34 -0400

    Edited folder names

--- src/test/Card.test.ts
+++ src/test/Card.test.ts
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, it } from 'vitest';
-import Card from '../ts/Card.js';
+import Card from '../Scripts/Card.js';
 
 describe('Tests for the Card class', () => {
   let CardTest: Card;
